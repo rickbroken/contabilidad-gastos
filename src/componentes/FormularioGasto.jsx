48b51e5ad89eec14dd34db0055803cc4ba857c62
ricardo20011 <daironquebrada@gmail.com>
2023-06-15 22:47:58 -0500

    Agregando formato de moneda a gastos

--- src/componentes/FormularioGasto.jsx
+++ src/componentes/FormularioGasto.jsx
@@ -11,7 +11,6 @@ import {useAuth} from './../contextos/AuthContext';
 import Alerta from './../elementos/Alerta';
 import { useNavigate } from 'react-router-dom';
 import editarGasto from './../firebase/editarGasto';
-import convertirAMoneda from './../funciones/convertirAMoneda';
 
 const FormularioGasto = ({gasto, gastoId}) => {
 
@@ -45,7 +44,7 @@ const FormularioGasto = ({gasto, gastoId}) => {
 		if(e.target.name === 'descripcion'){
 			cambiarInputDescripcion(e.target.value);
 		} else if(e.target.name === 'cantidad'){
-			cambiarInputCantidad(convertirAMoneda(e.target.value.replace(/[^0-9.]/g, '')));
+			cambiarInputCantidad(e.target.value.replace(/[^0-9.]/g, ''));
 		}
 	}
 
